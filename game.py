4d14c2f22bf78ef98832d00dfa8eba8058a04272
TagCopperLight <tagcopperlight@gmail.com>
2023-07-03 13:10:36 +0200

    Added Multiprocessing - DEBUG

--- game.py
+++ game.py
@@ -7,13 +7,15 @@ from random import shuffle
 
 
 class Game:
-    def __init__(self, players: list[Player], roles: list[Role], districts: list[District]) -> None:
+    def __init__(self, players: list[Player], roles: list[Role], districts: list[District], debug: bool = False) -> None:
         self.game_state = GameState(districts, roles)
         self.players = players
 
         self.first_to_finish = -1
 
         self.roles = roles
+
+        self.DEBUG = debug
 
     def init(self) -> None:
         shuffle(self.game_state.districts)
@@ -30,9 +32,8 @@ class Game:
         for id in list(range(self.game_state.current_player_id + 1, len(self.players))) + list(range(0, self.game_state.current_player_id)):
             players += [player for player in self.players if player.id == id]
 
-        self.players = players
-
     def select_characters(self) -> None:
+        #TODO: Enlever un role de plus et le rajouter à la fin
         roles = self.game_state.roles.copy()
         shuffle(roles)
         unavailable_characters = [roles.pop(), roles.pop()]
@@ -42,28 +43,97 @@ class Game:
         for player in self.players:
             available_characters = roles
             player.role = player.select_character(self.game_state, available_characters, unavailable_characters)
-            # print(f"Player {player.id} selects {player.role}", "\n")
             roles.remove(player.role)
-
+        
     def play(self) -> None:
         players = sorted(self.players, key=lambda player: player.role.order)
 
-        self.calculate_incomes()
-
         for player in players:
-            if player.choose_money_district(self.game_state):
-                # print(f"Player {player.id} ({player.role}) takes 2 gold", "\n")
-                player.money += self.game_state.bank.withdraw(2)
+            self.debug(f"Player {player.id}: {player.role}")
+        self.debug("")
+
+        while len(players) > 0:
+            player = players.pop(0)
+
+            # ----------- Roles ------------
+            if player.role.order == 0:
+                focused_role = player.action(self.game_state)[1]
+                focused_player = [player for player in self.players if player.role == focused_role]
+                self.debug(f"Player {player.id} ({player.role}) tried to kill {focused_role}")
+                if focused_player:
+                    self.debug(f"Player {player.id} ({player.role}) kills Player {focused_player[0].id} ({focused_player[0].role})")
+                    players.remove(focused_player[0])
+
+            elif player.role.order == 1:
+                focused_role = player.action(self.game_state)[1]
+                focused_player = [player for player in self.players if player.role == focused_role]
+                self.debug(f"Player {player.id} ({player.role}) tried to steal from {focused_role}")
+                if focused_player:
+                    self.debug(f"Player {player.id} ({player.role}) steals {focused_player[0].money} gold from Player {focused_player[0].id} ({focused_player[0].role})")
+                    player.money += focused_player[0].money
+                    focused_player[0].money = 0
+
+            elif player.role.order == 2:
+                # TODO: Focus a player not a role
+                focused_role = player.action(self.game_state)[1]
+                focused_player = [player for player in self.players if player.role == focused_role]
+                self.debug(f"Player {player.id} ({player.role}) tried to swap hands with {focused_role}")
+                if focused_player:
+                    self.debug(f"Player {player.id} ({player.role}) swaps hands with Player {focused_player[0].id} ({focused_player[0].role})")
+                    hand = focused_player[0].hand.copy()
+                    focused_player[0].hand = player.hand.copy()
+                    player.hand = hand
+
+            elif player.role.order == 3:
+                self.debug(f"Player {player.id} ({player.role}) gets the crown")
+                self.game_state.current_player_id = player.id
+                self.calculate_incomes(player, 1)
+            
+            elif player.role.order == 4:
+                self.calculate_incomes(player, 2)
+            
+            elif player.role.order == 5:
+                player.money += self.game_state.bank.withdraw(1)
+                self.calculate_incomes(player, 3)
+
+            elif player.role.order == 7:
+                self.calculate_incomes(player, 4)
+                # TODO: Focus a player not a role
+                focused_role = player.action(self.game_state)[1]
+                focused_player = [player for player in self.players if player.role == focused_role]
+                if focused_player and focused_player[0].citadel:
+                    district_to_destroy = min(focused_player[0].citadel, key=lambda district: district.cost)
+                    cost = district_to_destroy.cost
+                    if player.money < cost - 1:
+                        if len(focused_player[0].citadel) < 7 or focused_player[0].role.order != 4:
+                            player.money -= self.game_state.bank.deposit(cost - 1)
+                            self.debug(f"Player {player.id} ({player.role}) destroys {district_to_destroy} from Player {focused_player[0].id} ({focused_player[0].role})")
+                            focused_player[0].citadel.remove(district_to_destroy)
+
+            # ----------- Actions ------------
+            if player.role.order == 6:
+                if player.choose_money_district(self.game_state):
+                    self.debug(f"Player {player.id} ({player.role}) takes 4 gold", "\n")
+                    player.money += self.game_state.bank.withdraw(4)
+                else:
+                    self.debug(f"Player {player.id} ({player.role}) takes 2 card", "\n")
+                    for _ in range(2):
+                        if self.game_state.districts:
+                            player.hand.append(self.game_state.districts.pop())
             else:
-                # print(f"Player {player.id} ({player.role}) takes 1 card", "\n")
-                if self.game_state.districts:
-                    player.hand.append(self.game_state.districts.pop())
+                if player.choose_money_district(self.game_state):
+                    self.debug(f"Player {player.id} ({player.role}) takes 2 gold", "\n")
+                    player.money += self.game_state.bank.withdraw(2)
+                else:
+                    self.debug(f"Player {player.id} ({player.role}) takes 1 card", "\n")
+                    if self.game_state.districts:
+                        player.hand.append(self.game_state.districts.pop())
 
             to_build, _ = player.action(self.game_state)
 
             # TODO: Validate the build 
             if to_build.id != 0:
-                # print(f"Player {player.id} ({player.role}) builds {to_build}", "\n")
+                self.debug(f"Player {player.id} ({player.role}) built {to_build}", "\n")
                 player.money -= self.game_state.bank.deposit(to_build.cost)
 
                 player.hand.remove(to_build)
@@ -71,12 +141,10 @@ class Game:
                 if len(player.citadel) >= 7:
                     self.first_to_finish = player.id
 
-            # TODO: Role effects
-        
-    def calculate_incomes(self) -> None:
-        # TODO: District and role effects
-        for player in self.players:
-            player.money += self.game_state.bank.withdraw(0)
+    def calculate_incomes(self, player: Player, color: int) -> None:
+        for district in player.citadel:
+            if district.color == color:
+                player.money += self.game_state.bank.withdraw(1)
 
     def calculate_scores(self) -> list[tuple[Player, int]]:
         scores: list[tuple[Player, int]] = []
@@ -103,10 +171,17 @@ class Game:
 
     def game_over(self) -> tuple[bool, Player]:
         ended = any([len(player.citadel) >= 7 for player in self.players])
-        scores = [score for _, score in self.calculate_scores()]
-        if any(scores.count(x) > 1 for x in scores):
-            return ended, Player(-1)
-        return ended, max(self.calculate_scores(), key=lambda score: score[1])[0]
+        max_score = (Player(-1), 0)
+        if ended:
+            max_score = max(self.calculate_scores(), key=lambda score: score[1])
+            if len([score for score in self.calculate_scores() if score[1] == max_score[1]]) > 1:
+                max_score = (Player(-1), 0)
+
+        return ended, max_score[0]
     
     def __repr__(self) -> str:
         return f"Game(game_state={self.game_state}, players={self.players})"
+    
+    def debug(self, *args: int|str|Role) -> None:
+        if self.DEBUG:
+            print(*args)
--- main.py
+++ main.py
@@ -1,53 +1,111 @@
 from game import Game
 from data import ROLES, DISTRICTS
 from player import Player
+from basic_player import BasicPlayer #type: ignore
 
+import time
 from matplotlib import pyplot as plt
-from progress.bar import IncrementalBar #type: ignore
+import numpy as np
+from tqdm import tqdm
+
+from multiprocessing import Pool, current_process
 
 
-def play_game() -> tuple[int, list[tuple[Player, int]]]:
-    players = [Player(0), Player(1), Player(2), Player(3), Player(4)]
+DEBUG = False
+BAR = True
 
-    game = Game(players, ROLES, DISTRICTS)
+def debug(*args: str|int|Game) -> None:
+    if DEBUG:
+        print(*args)
+
+def graph(winner_list : list[int], score_list: list[int], turn_list: list[int], show: bool) -> None:
+    total_games = len(winner_list)
+    print(f"Total games: {total_games}")
+    print(f"Games/second: {total_games / (time.time() - start_time)}")
+    turn_array = np.array(turn_list)
+    print(f"Average turns: {np.mean(turn_array)}")
+
+    bins = [i + 0.5 for i in range(-1, 5)]
+    plt.hist(winner_list, bins = bins, edgecolor = 'black', histtype = 'bar') #type: ignore
+
+    plt.figure() #type: ignore
+    bins = [i + 0.5 for i in range(-1, max(score_list) + 1)]
+    plt.hist(score_list, bins = bins, edgecolor = 'black', histtype = 'bar') #type: ignore
+    print("--- %s seconds ---" % (time.time() - start_time), "\n")
+    if show:
+        plt.show() #type: ignore
+
+
+def play_game() -> tuple[int, list[tuple[Player, int]], int]:
+    players: list[Player] = [Player(0)] + [Player(i) for i in range(1, 5)]
+
+    game = Game(players, ROLES, DISTRICTS, DEBUG)
     game.init()
+    debug(game, "\n")
 
     turn = 0
 
     while True:
+        debug("----------- Turn", turn, "----------- \n")
         game.select_characters()
         game.play()
+        debug("")
+        debug(game, "\n")
 
         if game.game_over()[0]:
-            return game.game_over()[1].id, game.calculate_scores()
+            return game.game_over()[1].id, game.calculate_scores(), turn
 
         turn += 1
 
-winner_list: list[int] = []
-score_list: list[int] = []
 
-ties = 0
+def process(iterations: int) -> tuple[list[int], list[int], list[int]]:
+    winner_list: list[int] = []
+    score_list: list[int] = []
+    turn_list: list[int] = []
 
-iterations = 10000
-with IncrementalBar('Playing games', max = iterations) as bar:
-    for i in range(iterations):
-        winner, scores = play_game()
+    process_id = current_process()._identity[0]
 
-        scores = [score for _, score in scores]
+    if BAR and process_id == 1:
+        for _ in tqdm(range(iterations)):
+            winner, scores, turns = play_game()
 
-        winner_list.append(winner)
-        score_list += scores
+            scores = [score for _, score in scores]
 
-        if winner == -1:
-            ties += 1
+            winner_list.append(winner)
+            turn_list.append(turns)
+            score_list += scores
+    else:
+        for _ in range(iterations):
+            winner, scores, turns = play_game()
 
-        bar.next()
+            scores = [score for _, score in scores]
 
-bins = [i + 0.5 for i in range(-1, 5)]
-plt.hist(winner_list, bins = bins, edgecolor = 'black', histtype = 'bar') #type: ignore
-print(f"Ties: {ties}")
+            winner_list.append(winner)
+            turn_list.append(turns)
+            score_list += scores
 
-plt.figure() #type: ignore
-bins = [i + 0.5 for i in range(-1, max(score_list) + 1)]
-plt.hist(score_list, bins = bins, edgecolor = 'black', histtype = 'bar') #type: ignore
-plt.show() #type: ignore
+    return winner_list, score_list, turn_list
+
+def worker(iterations: int) -> tuple[list[int], list[int], list[int]]:
+    p = process(iterations)
+    return p
+
+def main(iterations:int, processes:int) -> None:
+    with Pool(processes=processes) as pool: #type: ignore
+        results = pool.map(worker, [iterations//processes for _ in range(processes)], chunksize=1)
+        results += pool.map(worker, [iterations%processes], chunksize=1)
+        pool.close()
+
+    winner_list: list[int] = []
+    score_list: list[int] = []
+    turn_list: list[int] = []
+
+    for result in results:
+        winner_list += result[0]
+        score_list += result[1]
+        turn_list += result[2]
+
+    graph(winner_list, score_list, turn_list, True)
+
+start_time = time.time()
+main(1000000, 6)
