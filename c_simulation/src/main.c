ca796443a466324fb2a45c734054aa029ee2fecb
TagCopperLight <tagcopperlight@gmail.com>
2023-07-03 15:33:33 +0200

    src folder

---
+++ c_simulation/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    printf("c = %d\n", c);
+    scanf("%d", &a);
+    printf("a = %d\n", a);
+    return 0;
+}
