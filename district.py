9e8ae50b832b1e6236344422c335c28bfa60fab1
TagCopperLight <tagcopperlight@gmail.com>
2023-07-03 13:09:37 +0200

    Changed repr of district

--- district.py
+++ district.py
@@ -7,5 +7,5 @@ class District:
     
     def __repr__(self) -> str:
         # return f"District(id={self.id}, name={self.name}, cost={self.cost}, color={self.color})"
-        return f"District({self.id}, {self.name})"
+        return f"District({self.id}, {self.name}, {self.cost})"
         # return ""
